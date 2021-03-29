edec545faaa9930d9aba10e856a1641781866c77
DamirAhm <damir221003@gmail.com>
2021-03-29 16:29:36 +0400

    Added favicon

--- server.js
+++ server.js
@@ -26,123 +26,129 @@ const executablePath =
 		: path.resolve(__dirname, './node_modules/puppeteer/.local-chromium/chrome-win/chrome.exe');
 
 (async () => {
-	const app = fastify();
-	await app.register(fastifyExpress);
+	try {
+		const app = fastify();
+		await app.register(fastifyExpress);
 
-	app.use(cors());
-	app.use(
-		sirv(path.join(__dirname, 'dist'), {
-			maxAge: 31536000,
-			immutable: true,
-			gzip: true,
-		}),
-	);
-	app.use(compression());
+		app.use(cors());
+		app.use(
+			sirv(path.join(__dirname, 'dist'), {
+				maxAge: 31536000,
+				immutable: true,
+				gzip: true,
+			}),
+		);
+		app.use(compression());
 
-	const browser = await pup.launch({
-		executablePath,
-		args: ['--no-sandbox'],
-	});
+		const browser = await pup.launch({
+			executablePath,
+			args: ['--no-sandbox'],
+		});
 
-	app.get('/api/subjects', async (req, res) => {
-		res.type(responseTypes.json);
-		return await getSubjects(browser);
-	});
-	app.get('/api/topics/:subjectPrefix', async (req, res) => {
-		try {
-			const { subjectPrefix } = req.params;
-
+		app.get('/api/subjects', async (req, res) => {
 			res.type(responseTypes.json);
+			return await getSubjects(browser);
+		});
+		app.get('/api/topics/:subjectPrefix', async (req, res) => {
+			try {
+				//@ts-ignore
+				const { subjectPrefix } = req.params;
 
-			let topics;
-			if (topicsCache.has(subjectPrefix)) topics = topicsCache.get(subjectPrefix);
-			else {
-				topics = await getTopics(getUrlFromPrefix(subjectPrefix));
+				res.type(responseTypes.json);
 
-				if (topics) {
-					topicsCache.set(subjectPrefix, topics);
-				}
-			}
+				let topics;
+				if (topicsCache.has(subjectPrefix)) topics = topicsCache.get(subjectPrefix);
+				else {
+					topics = await getTopics(getUrlFromPrefix(subjectPrefix));
 
-			if (!topics) {
-				res.code(404);
-				throw new Error();
-			}
-
-			return topics.map(({ subtopics, ...rest }) => rest);
-		} catch (e) {
-			console.error(e);
-			res.code(404);
-			throw new Error();
-		}
-	});
-	app.get('/api/tasks/:subjectPrefix/:issue', async (req, res) => {
-		try {
-			//@ts-ignore
-			const { subjectPrefix, issue } = req.params;
-			//@ts-ignore
-			const { amount = 5, usedProblemsString = '[]' } = req.query;
-			const used = usedProblemsString.split(',');
-
-			let topics;
-			if (topicsCache.has(subjectPrefix)) topics = topicsCache.get(subjectPrefix);
-			else {
-				topics = await getTopics(getUrlFromPrefix(subjectPrefix));
-				topicsCache.set(subjectPrefix, topics);
-			}
-
-			if (topics) {
-				const issues = issue.split('+');
-
-				const result = new Map();
-
-				for (const issue of issues) {
-					const topic = topics?.find((topic) => topic.issue === issue);
-
-					if (!topic) {
-						continue;
-					}
-
-					const urlSet =
-						topic.subtopics != null ? getUrlSetFromTopic(topic) : [topic.url];
-
-					const tasks = await loadTasksFromPage({ urlSet, amount, used }, browser);
-
-					if (tasks && tasks.length > 0) {
-						result.set(issue, tasks);
+					if (topics) {
+						topicsCache.set(subjectPrefix, topics);
 					}
 				}
 
-				if (result.size === 0) {
+				if (!topics) {
 					res.code(404);
 					throw new Error();
 				}
 
-				res.type(responseTypes.json);
-
-				const resultTasks = [];
-
-				for (const [issue, tasks] of result) {
-					const tasksWithIssue = tasks.map((task) => ({ ...task, issue }));
-					resultTasks.push(...tasksWithIssue);
-				}
-
-				return resultTasks;
-			} else {
+				return topics.map(({ subtopics, ...rest }) => rest);
+			} catch (e) {
+				console.error(e);
 				res.code(404);
 				throw new Error();
 			}
-		} catch (e) {
-			console.error(e);
+		});
+		app.get('/api/tasks/:subjectPrefix/:issue', async (req, res) => {
+			try {
+				//@ts-ignore
+				const { subjectPrefix, issue } = req.params;
+				//@ts-ignore
+				const { amount = 5, usedProblemsString = '[]' } = req.query;
+				const used = usedProblemsString.split(',');
 
-			res.code(404);
-			throw new Error();
-		}
-	});
-	app.get('/*', (_, res) => {
-		const stream = fs.createReadStream(path.join(__dirname, 'build/index.html'));
-		res.type('text/html').send(stream);
-	});
+				let topics;
+				if (topicsCache.has(subjectPrefix)) topics = topicsCache.get(subjectPrefix);
+				else {
+					topics = await getTopics(getUrlFromPrefix(subjectPrefix));
+					topicsCache.set(subjectPrefix, topics);
+				}
 
-	app.listen(process.env.PORT ?? 3000).then(() => console.log('Server listening'));
+				if (topics) {
+					const issues = issue.split('+');
+
+					const result = new Map();
+
+					for (const issue of issues) {
+						const topic = topics?.find((topic) => topic.issue === issue);
+
+						if (!topic) {
+							continue;
+						}
+
+						const urlSet =
+							topic.subtopics != null ? getUrlSetFromTopic(topic) : [topic.url];
+
+						const tasks = await loadTasksFromPage({ urlSet, amount, used }, browser);
+
+						if (tasks && tasks.length > 0) {
+							result.set(issue, tasks);
+						}
+					}
+
+					if (result.size === 0) {
+						res.code(404);
+						throw new Error();
+					}
+
+					res.type(responseTypes.json);
+
+					const resultTasks = [];
+
+					for (const [issue, tasks] of result) {
+						const tasksWithIssue = tasks.map((task) => ({ ...task, issue }));
+						resultTasks.push(...tasksWithIssue);
+					}
+
+					return resultTasks;
+				} else {
+					res.code(404);
+					throw new Error();
+				}
+			} catch (e) {
+				console.error(e);
+
+				res.code(404);
+				throw new Error();
+			}
+		});
+
+		app.get('/*', (_, res) => {
+			const stream = fs.createReadStream(path.join(__dirname, './dist/index.html'));
+			res.type('text/html').send(stream);
+		});
+
+		app.listen(process.env.PORT ?? 3000).then(() => console.log('Server listening'));
+	} catch (e) {
+		console.error(e);
+	}
 })();
